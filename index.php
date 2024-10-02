38f64111459b04368293cf999caf39a03fb96cd1
susmita <susmitaghosh.leelija@gmail.com>
2024-10-02 11:45:33 +0530

    employee details

--- index.php
+++ index.php
@@ -1,13 +1,24 @@
 <?php
 $con = mysqli_connect("localhost", "root", "", "leelija_db");
 
-function getEmployeeDetails($id){
-    global $con;
-    $sql = "SELECT * FROM employees WHERE emp_id ='$id'";
-    $rs = $con->query($sql);
-    $result = $rs->fetch_assoc();
-    print_r($result);
+
+
+if (isset($_REQUEST['action'])) {
+    $action = $_REQUEST['action'];
+
+    if ($action == 'getEmployeeDetails') {
+        $id = $_REQUEST['emp_id'];
+        $sql = "SELECT * FROM employees WHERE emp_id ='$id'";
+        $rs = $con->query($sql);
+        $result = $rs->fetch_assoc();
+        if ($result) {
+            $response = array('success' => true, 'message' => 'Employee Details Fetched successfully', 'data' => $result);
+            echo json_encode($response);
+            die();
+        } else {
+            $response = array('success' => false, 'message' => 'Failed to fetch details');
+            echo json_encode($response);
+            die();
+        }
+    }
 }
-
-?>
-
