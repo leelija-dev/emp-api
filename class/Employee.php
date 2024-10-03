c6e233a8ed2b8a6f8b62d99687953dbb88781b3b
susmita <susmitaghosh.leelija@gmail.com>
2024-10-03 14:28:03 +0530

    add update doc

--- class/Employee.php
+++ class/Employee.php
@@ -27,45 +27,108 @@ class Employee extends DatabaseConnection
         }
     }
 
-    public function addEmployeeDoc($id, $doc_name, $doc_path, $updated_by)
+
+    public function getDocDetails($docId){
+        $query = "SELECT * FROM emp_docs WHERE id = ?";
+        $stmt = mysqli_prepare($this->conn, $query);
+
+        if ($stmt) {
+            mysqli_stmt_bind_param($stmt, 'i', $docId);
+            mysqli_stmt_execute($stmt);
+
+            $result = mysqli_stmt_get_result($stmt);
+            $employee = mysqli_fetch_assoc($result);
+            return $employee;
+        }
+    }
+
+    public function getEmployeesName()
     {
+        $query = "SELECT name FROM employees";
+        $stmt = mysqli_prepare($this->conn, $query);
 
-        $sql = "INSERT INTO emp_docs (emp_id, doc_name, doc_path, updated_by) VALUES (?, ?, ?, ?)";
-        $stmt = mysqli_prepare($this->conn, $sql);
-        if ($stmt){
-            mysqli_stmt_bind_param($stmt, 'isss', $id, $doc_name, $doc_path, $updated_by);
-
-            if (mysqli_stmt_execute($stmt)) {
-
-                $response = array('success' => true, 'message' => 'Employee Documents Submitted successfully');
+        if ($stmt) {
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+           
+            $employees = mysqli_fetch_all($result, MYSQLI_ASSOC);
+            if ($employees) {
+                $response = array('success' => true, 'message' => 'Employee Names Fetched successfully', 'data' => $employees);
                 echo json_encode($response);
                 die();
             } else {
-                $response = array('success' => false, 'message' => 'Failed to Submit documents');
+                $response = array('success' => false, 'message' => 'Failed to fetch details');
                 echo json_encode($response);
                 die();
             }
         }
     }
 
-    public function updateEmployeeDoc($id, $doc_name, $doc_path, $updated_by)
+    public function getEmployeesDetails()
     {
+        $query = "SELECT * FROM employees";
+        $stmt = mysqli_prepare($this->conn, $query);
 
-        $sql = "INSERT INTO emp_docs (emp_id, doc_name, doc_path, updated_by) VALUES (?, ?, ?, ?)";
-        $stmt = mysqli_prepare($this->conn, $sql);
         if ($stmt) {
-            mysqli_stmt_bind_param($stmt, 'isss', $id, $doc_name, $doc_path, $updated_by);
-
-            if (mysqli_stmt_execute($stmt)) {
-
-                $response = array('success' => true, 'message' => 'Employee Documents Submitted successfully');
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+           
+            $employees = mysqli_fetch_all($result, MYSQLI_ASSOC);
+            if ($employees) {
+                $response = array('success' => true, 'message' => 'Employees Details Fetched successfully', 'data' => $employees);
                 echo json_encode($response);
                 die();
             } else {
-                $response = array('success' => false, 'message' => 'Failed to Submit documents');
+                $response = array('success' => false, 'message' => 'Failed to fetch details');
                 echo json_encode($response);
                 die();
             }
+        }
+    }
+
+
+    // public function addEmployeeDoc($id, $doc_name, $doc_path, $updated_by)
+    // {
+
+    //     $sql = "INSERT INTO emp_docs (emp_id, doc_name, doc_path, updated_by) VALUES (?, ?, ?, ?)";
+    //     $stmt = mysqli_prepare($this->conn, $sql);
+    //     if ($stmt){
+    //         mysqli_stmt_bind_param($stmt, 'isss', $id, $doc_name, $doc_path, $updated_by);
+
+    //         if (mysqli_stmt_execute($stmt)) {
+
+    //             $response = array('success' => true, 'message' => 'Employee Documents Submitted successfully');
+    //             echo json_encode($response);
+    //             die();
+    //         } else {
+    //             $response = array('success' => false, 'message' => 'Failed to Submit documents');
+    //             echo json_encode($response);
+    //             die();
+    //         }
+    //     }
+    // }
+
+    public function updateEmployeeDoc($doc_id, $emp_id, $doc_name, $doc_path, $updated_by)
+    {
+        $sql = "UPDATE emp_docs SET emp_id = ?, doc_name = ?, doc_path = ?, updated_by = ? WHERE id = ?";
+        $stmt = mysqli_prepare($this->conn, $sql);
+    
+        if ($stmt) {
+            mysqli_stmt_bind_param($stmt, 'isssi', $emp_id, $doc_name, $doc_path, $updated_by, $doc_id);
+    
+            if (mysqli_stmt_execute($stmt)) {
+                $response = array('success' => true, 'message' => 'Employee Document updated successfully');
+                echo json_encode($response);
+                die();
+            } else {
+                $response = array('success' => false, 'message' => 'Failed to update document');
+                echo json_encode($response);
+                die();
+            }
+        } else {
+            $response = array('success' => false, 'message' => 'Failed to prepare the query');
+            echo json_encode($response);
+            die();
         }
     }
 }
--- index.php
+++ index.php
@@ -7,30 +7,76 @@ $segments = explode('/', $url_path);
 $first_segment = isset($segments[0]) ? $segments[0] : null;
 $second_segment = isset($segments[1]) ? $segments[1] : null;
 $third_segment = isset($segments[2]) ? $segments[2] : null;
+$forth_segment = isset($segments[3]) ? $segments[3] : null;
 $Employee = new Employee();
 
 if (isset($first_segment)) {
     if ($second_segment == 'employees' && is_numeric($third_segment)) {
         $id = $third_segment;
+        
         $response = $Employee->getEmployeeDetails($id);
         echo $response;
     }
 
-    if ($action == 'postEmployeeDetails') {
-        $id = $_REQUEST['emp_id'];
-        $updated_by = $_REQUEST['updated_by'];
-        if (isset($_FILES['doc_name']) && $_FILES['doc_name']['error'] == UPLOAD_ERR_OK) {
-            $doc_name = $_FILES['doc_name']['name'];
-            $doc_tmp_path = $_FILES['doc_name']['tmp_name'];
-            $target_directory = "EmployeeDoc/";
-            $doc_path = $target_directory . basename($doc_name);
+    //ADD DOCUMENT IN THE TABLE
 
-            if (move_uploaded_file($doc_tmp_path, $doc_path)) {
-                $response = $Employee->addEmployeeDoc($id, $doc_name, $doc_path, $updated_by);
-                echo $response;
-            } else {
-                echo "Error moving the uploaded file.";
+
+    // if ($action == 'postEmployeeDetails') {
+    //     $id = $_REQUEST['emp_id'];
+    //     $updated_by = $_REQUEST['updated_by'];
+    //     if (isset($_FILES['doc_name']) && $_FILES['doc_name']['error'] == UPLOAD_ERR_OK) {
+    //         $doc_name = $_FILES['doc_name']['name'];
+    //         $doc_tmp_path = $_FILES['doc_name']['tmp_name'];
+    //         $target_directory = "EmployeeDoc/";
+    //         $doc_path = $target_directory . basename($doc_name);
+
+    //         if (move_uploaded_file($doc_tmp_path, $doc_path)) {
+    //             $response = $Employee->addEmployeeDoc($id, $doc_name, $doc_path, $updated_by);
+    //             echo $response;
+    //         } else {
+    //             echo "Error moving the uploaded file.";
+    //         }
+    //     }
+    // }
+
+//This section is for all employee name
+    if ($second_segment == 'employees' && $third_segment == null && $forth_segment == null) {
+        $response = $Employee->getEmployeesName();
+        echo $response;
+    }
+
+    //This section is for all employee details
+    if ($second_segment == 'employee-details' && $third_segment == null && $forth_segment == null) {
+        $response = $Employee->getEmployeesDetails();
+        echo $response;
+    }
+
+    //UPDATE EMPLOYEE DETAILS USING DOCUMEMT ID
+    if ($second_segment == 'emp' && $third_segment == 'update' && is_numeric($forth_segment)) {
+        $id = $forth_segment;
+            $emp_id = $_POST['emp_id'];
+            $updated_by = $_REQUEST['updated_by'];
+            if (isset($_FILES['doc_name']) && $_FILES['doc_name']['error'] == UPLOAD_ERR_OK) {
+                $doc_name = $_FILES['doc_name']['name'];
+                $doc_tmp_path = $_FILES['doc_name']['tmp_name'];
+                $target_directory = "EmployeeDoc/";
+                $doc_path = $target_directory . basename($doc_name);
+    
+                if (move_uploaded_file($doc_tmp_path, $doc_path)) {
+                    $response = $Employee->updateEmployeeDoc($id, $emp_id, $doc_name, $doc_path, $updated_by);
+                    echo $response;
+                } else {
+                    echo "Error moving the uploaded file.";
+                }
+            }
+            else{
+                $data = $Employee->getDocDetails($id);
+                $doc_name = $data['doc_name'];
+                $doc_path = $data['doc_path'];
+                $response = $Employee->updateEmployeeDoc($id, $emp_id, $doc_name, $doc_path, $updated_by);
+                    echo $response;
             }
         }
-    }
+
+    
 }
