86ba6953dd55be91fed1293f46760dcbd8c4b7ac
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2024-10-05 11:26:00 +0530

    Headers added

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
 require_once 'class/Employee.php';
 $url = $_SERVER['REQUEST_URI'];
 $url_path = ltrim($url, '/');
@@ -135,4 +140,7 @@ if (isset($first_segment)) {
             }
         } 
     }
+}else {   
+    header("HTTP/1.1 404 Not Found");
+    exit();
 }
